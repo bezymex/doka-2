c4d96e8b4ee417bbbd6537b5c56b93257e0118e7
Evgeny Tulupov <tulupovzena@gmail.com>
2021-03-28 19:25:34 +0300

    wall

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Object(pygame.sprite.Sprite):
 RES=WIDTH,HEIGHT=800,600
 
 # точка спавна игрока 
-start_x = 100
-start_y = 120
-start1_x = 200
-start1_y = 200
+start_x = 630
+start_y = 500
+start1_x = 700
+start1_y = 500
 
 
 
@@ -31,11 +31,13 @@ pygame.display.set_caption("Doka 2")
 #импорт изображений 
 bg=pygame.transform.scale(pygame.image.load("images/фо.jpg"), (RES))
 player_img=pygame.transform.scale(pygame.image.load("images/player.png"), (64,64))
-player1_img=pygame.transform.scale(pygame.image.load("images/pudge.png"), (64,64))
+player1_img=pygame.transform.scale(pygame.image.load("images/io.png"), (80,80))
+wall=pygame.transform.scale(pygame.image.load("images/wall.png"), (80,80))
 
 
 # создание групп объектов
 all_sprites = pygame.sprite.Group()
+walls = pygame.sprite.Group()
 
 
 #создание объектов 
@@ -43,6 +45,27 @@ player = Object(player_img, start_x, start_y, 4)
 all_sprites.add(player)
 player1 = Object(player1_img, start1_x, start1_y, 4)
 all_sprites.add(player1)
+
+#создание стен Оbjесt
+wall1 = Object(wall, 500,500,0)
+wall2 = Object(wall, 270,420,0)
+wall3 = Object(wall, 710,345,0)
+wall4 = Object(wall, 620,330,0)
+wall5 = Object(wall, 510,360,0)
+wall6 = Object(wall, 350,405,0)
+wall7 = Object(wall, 170,405,0)
+wall8 = Object(wall, 80,405,0)
+wall9 = Object(wall, 170,320,0)
+wall10 = Object(wall, 390,210,0)
+wall11 = Object(wall, 410,140,0)
+wall12 = Object(wall, 396,70,0)
+wall13 = Object(wall, 405,10,0)
+wall14 = Object(wall, 560,150,0)
+wall15 = Object(wall, 490,140,0)
+wall16 = Object(wall, 170,250,0)
+
+walls.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16)
+all_sprites.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16)
 
 run = True
 while run:
