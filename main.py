3eb85fa0dd7047484457aebd8f2a75faef14621f
Evgeny Tulupov <tulupovzena@gmail.com>
2021-04-04 19:07:52 +0300

    added enemies

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ RES=WIDTH,HEIGHT=800,600
 
 # точка спавна игрока 
 start_x = 630
-start_y = 500
+start_y = 450
 start1_x = 700
-start1_y = 500
+start1_y = 450
 
 
 
@@ -30,14 +30,16 @@ pygame.display.set_caption("Doka 2")
 
 #импорт изображений 
 bg=pygame.transform.scale(pygame.image.load("images/фо.jpg"), (RES))
-player_img=pygame.transform.scale(pygame.image.load("images/player.png"), (64,64))
-player1_img=pygame.transform.scale(pygame.image.load("images/io.png"), (80,80))
-wall=pygame.transform.scale(pygame.image.load("images/wall.png"), (80,80))
+player_img=pygame.transform.scale(pygame.image.load("images/player.png"), (50,50))
+player1_img=pygame.transform.scale(pygame.image.load("images/io.png"), (50,50))
+wall=pygame.transform.scale(pygame.image.load("images/wall.png"), (60,60))
+enemy_img=pygame.transform.scale(pygame.image.load("images/Techies.png"), (100,100))
 
 
 # создание групп объектов
 all_sprites = pygame.sprite.Group()
 walls = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 
 
 #создание объектов 
@@ -47,25 +49,68 @@ player1 = Object(player1_img, start1_x, start1_y, 4)
 all_sprites.add(player1)
 
 #создание стен Оbjесt
-wall1 = Object(wall, 500,500,0)
-wall2 = Object(wall, 270,420,0)
-wall3 = Object(wall, 710,345,0)
-wall4 = Object(wall, 620,330,0)
-wall5 = Object(wall, 510,360,0)
-wall6 = Object(wall, 350,405,0)
-wall7 = Object(wall, 170,405,0)
-wall8 = Object(wall, 80,405,0)
-wall9 = Object(wall, 170,320,0)
-wall10 = Object(wall, 390,210,0)
-wall11 = Object(wall, 410,140,0)
-wall12 = Object(wall, 396,70,0)
-wall13 = Object(wall, 405,10,0)
-wall14 = Object(wall, 560,150,0)
-wall15 = Object(wall, 490,140,0)
-wall16 = Object(wall, 170,250,0)
+wall1 = Object(wall, 0,0,0)
+wall2 = Object(wall, 60,0,0)
+wall3 = Object(wall, 0,60,0)
+wall4 = Object(wall, 120,0,0)
+wall5 = Object(wall, 0,120,0)
+wall6 = Object(wall, 180,0,0)
+wall7 = Object(wall, 0,180,0)
+wall8 = Object(wall, 240,0,0)
+wall9 = Object(wall, 0,240,0)
+wall10 = Object(wall, 300,0,0)
+wall11 = Object(wall, 0,300,0)
+wall12 = Object(wall, 360,0,0)
+wall13 = Object(wall, 0,360,0)
+wall14 = Object(wall, 420,0,0)
+wall15 = Object(wall, 0,420,0)
+wall16 = Object(wall, 480,0,0)
+wall17 = Object(wall, 0,480,0)
+wall18 = Object(wall, 540,0,0)
+wall19 = Object(wall, 600,0,0)
+wall21 = Object(wall, 0,540,0) 
+wall20 = Object(wall, 660,0,0)
+wall22 = Object(wall, 720,0,0)
+wall23 = Object(wall, 780,0,0)
+wall24 = Object(wall, 780,60,0)
+wall25 = Object(wall, 780,120,0)
+wall26 = Object(wall, 780,180,0)
+wall27 = Object(wall, 780,240,0)
+wall28 = Object(wall, 780,300,0)
+wall29 = Object(wall, 780,300,0)
+wall30 = Object(wall, 780,360,0)
+wall31 = Object(wall, 780,420,0)
+wall32 = Object(wall, 780,480,0)
+wall33 = Object(wall, 780,540,0)
+wall34 = Object(wall, 60,540,0)
+wall35 = Object(wall, 120,540,0)
+wall36 = Object(wall, 180,540,0)
+wall37 = Object(wall, 240,540,0)
+wall38 = Object(wall, 300,540,0)
+wall39 = Object(wall, 360,540,0)
+wall40 = Object(wall, 420,540,0)
+wall41 = Object(wall, 480,540,0)
+wall42 = Object(wall, 540,540,0)
+wall43 = Object(wall, 600,540,0)
+wall44 = Object(wall, 660,540,0)
+wall45 = Object(wall, 720,540,0)
 
-walls.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16)
-all_sprites.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16)
+
+enemy1_x=430
+enemy1_y=250
+enemy1=Object(enemy_img,enemy1_x,enemy1_y,2)
+enemy2_x=500
+enemy2_y=300
+enemy2=Object(enemy_img,enemy2_x,enemy2_y,2)
+enemy3_x=250
+enemy3_y=230
+enemy3=Object(enemy_img,enemy3_x,enemy3_y,2)
+
+
+
+walls.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22,wall23,wall24,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32,wall33,wall34,wall35,wall36,wall37,wall38,wall39,wall40,wall41,wall42,wall43,wall44,wall45)
+all_sprites.add(enemy1,enemy2,enemy3,wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22,wall23,wall24,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32,wall33,wall34,wall35,wall36,wall37,wall38,wall39,wall40,wall41,wall42,wall43,wall44,wall45)
+enemies.add(enemy1,enemy2,enemy3)
 
 run = True
 while run:
@@ -87,7 +132,25 @@ while run:
             player.image = pygame.transform.flip(player_img, True, False)
             player.rect.x -= player.speed
         keys =pygame.key.get_pressed()
-    
+
+
+    if len(pygame.sprite.spritecollide(player1, walls,enemies, False)) > 0:
+        player1.rect.x = start1_x
+        player1.rect.y = start1_y
+    if len(pygame.sprite.spritecollide(player, walls,enemies, False)) > 0:
+        player.rect.x = start_x
+        player.rect.y = start_y
+
+    enemy1.rect.y += enemy1.speed
+    if len(pygame.sprite.pygame.sprite.spritecollide(enemy1, walls, False)) > 0:
+        enemy1.speed *= -2
+    enemy2.rect.x -= enemy2.speed
+    if len(pygame.sprite.pygame.sprite.spritecollide(enemy2, walls, False)) > 0:
+        enemy2.speed *= -2
+    enemy3.rect.x += enemy3.speed
+    if len(pygame.sprite.pygame.sprite.spritecollide(enemy3, walls, False)) > 0:
+        enemy2.speed *= -2
+
     if event.type == pygame.KEYDOWN:
         if keys[pygame.K_UP]:
             player1.rect.y -= player1.speed
