f963f889533e87c7278cfbf6d92659c96c87fb14
Evgeny Tulupov <tulupovzena@gmail.com>
2021-04-11 19:08:19 +0300

    addit coins

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Object(pygame.sprite.Sprite):
 RES=WIDTH,HEIGHT=800,600
 
 # точка спавна игрока 
-start_x = 630
-start_y = 450
-start1_x = 700
+start1_x = 630
 start1_y = 450
+start2_x = 700
+start2_y = 450
 
 
 
@@ -30,23 +30,24 @@ pygame.display.set_caption("Doka 2")
 
 #импорт изображений 
 bg=pygame.transform.scale(pygame.image.load("images/фо.jpg"), (RES))
-player_img=pygame.transform.scale(pygame.image.load("images/player.png"), (50,50))
-player1_img=pygame.transform.scale(pygame.image.load("images/io.png"), (50,50))
+player1_img=pygame.transform.scale(pygame.image.load("images/player.png"), (50,50))
+player2_img=pygame.transform.scale(pygame.image.load("images/io.png"), (50,50))
 wall=pygame.transform.scale(pygame.image.load("images/wall.png"), (60,60))
 enemy_img=pygame.transform.scale(pygame.image.load("images/Techies.png"), (100,100))
-
-
+coin1_img=pygame.transform.scale(pygame.image.load("images/coin1.png"), (45,40))
+coin2_img=pygame.transform.scale(pygame.image.load("images/coin2.png"), (45,60))
+coin3_img=pygame.transform.scale(pygame.image.load("images/coin3.png"), (80,60))
 # создание групп объектов
 all_sprites = pygame.sprite.Group()
 walls = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
-
+items = pygame.sprite.Group()
 
 #создание объектов 
-player = Object(player_img, start_x, start_y, 4)
-all_sprites.add(player)
 player1 = Object(player1_img, start1_x, start1_y, 4)
 all_sprites.add(player1)
+player2 = Object(player2_img, start2_x, start2_y, 4)
+all_sprites.add(player2)
 
 #создание стен Оbjесt
 wall1 = Object(wall, 0,0,0)
@@ -95,7 +96,7 @@ wall43 = Object(wall, 600,540,0)
 wall44 = Object(wall, 660,540,0)
 wall45 = Object(wall, 720,540,0)
 
-
+#враги
 enemy1_x=430
 enemy1_y=250
 enemy1=Object(enemy_img,enemy1_x,enemy1_y,2)
@@ -106,13 +107,24 @@ enemy3_x=250
 enemy3_y=230
 enemy3=Object(enemy_img,enemy3_x,enemy3_y,2)
 
-
+#очки
+coin1_x=100
+coin1_y=100
+coin1=Object(coin1_img,coin1_x,coin1_y,0)
+coin2_x=250
+coin2_y=100
+coin2=Object(coin2_img,coin2_x,coin2_y,0)
+coin3_x=400
+coin3_y=100
+coin3=Object(coin3_img,coin3_x,coin3_y,0)
 
 walls.add(wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22,wall23,wall24,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32,wall33,wall34,wall35,wall36,wall37,wall38,wall39,wall40,wall41,wall42,wall43,wall44,wall45)
-all_sprites.add(enemy1,enemy2,enemy3,wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22,wall23,wall24,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32,wall33,wall34,wall35,wall36,wall37,wall38,wall39,wall40,wall41,wall42,wall43,wall44,wall45)
+all_sprites.add(coin1,coin2,coin3,enemy1,enemy2,enemy3,wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wall20,wall21,wall22,wall23,wall24,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32,wall33,wall34,wall35,wall36,wall37,wall38,wall39,wall40,wall41,wall42,wall43,wall44,wall45)
 enemies.add(enemy1,enemy2,enemy3)
+items.add(coin1,coin2,coin3)
 
 run = True
+points=0
 while run:
     screen.blit(bg, (0,0))
     for event in pygame.event.get():
@@ -122,46 +134,60 @@ while run:
     keys =pygame.key.get_pressed()
     if event.type == pygame.KEYDOWN:
         if keys[pygame.K_w]:
-            player.rect.y -= player.speed
+            player1.rect.y -= player1.speed
         if keys[pygame.K_s]:
-            player.rect.y += player.speed
+            player1.rect.y += player1.speed
         if keys[pygame.K_d]:
-            player.image = pygame.transform.flip(player_img, False, False)
-            player.rect.x += player.speed
+            player1.image = pygame.transform.flip(player1_img, False, False)
+            player1.rect.x += player1.speed
         if keys[pygame.K_a]:
-            player.image = pygame.transform.flip(player_img, True, False)
-            player.rect.x -= player.speed
+            player1.image = pygame.transform.flip(player1_img, True, False)
+            player1.rect.x -= player1.speed
         keys =pygame.key.get_pressed()
 
 
-    if len(pygame.sprite.spritecollide(player1, walls,enemies, False)) > 0:
+    if len(pygame.sprite.spritecollide(player2, walls, False)) > 0:
+        player2.rect.x = start2_x
+        player2.rect.y = start2_y
+    if len(pygame.sprite.spritecollide(player1, walls, False)) > 0:
         player1.rect.x = start1_x
         player1.rect.y = start1_y
-    if len(pygame.sprite.spritecollide(player, walls,enemies, False)) > 0:
-        player.rect.x = start_x
-        player.rect.y = start_y
+
+    if len(pygame.sprite.spritecollide(player2, enemies, False)) > 0:
+        player2.rect.x = start2_x
+        player2.rect.y = start2_y
+    if len(pygame.sprite.spritecollide(player1, enemies, False)) > 0:
+        player1.rect.x = start1_x
+        player1.rect.y = start1_y        
 
     enemy1.rect.y += enemy1.speed
     if len(pygame.sprite.pygame.sprite.spritecollide(enemy1, walls, False)) > 0:
-        enemy1.speed *= -2
-    enemy2.rect.x -= enemy2.speed
+        enemy1.speed *= -1
+    enemy2.rect.x += enemy2.speed
     if len(pygame.sprite.pygame.sprite.spritecollide(enemy2, walls, False)) > 0:
-        enemy2.speed *= -2
+        enemy2.speed *= -1
     enemy3.rect.x += enemy3.speed
     if len(pygame.sprite.pygame.sprite.spritecollide(enemy3, walls, False)) > 0:
-        enemy2.speed *= -2
+        enemy3.speed *= -1
+
+    if len(pygame.sprite.pygame.sprite.spritecollide(player1, items, True)) > 0:
+        points += 1
+        print(points)
+    if len(pygame.sprite.pygame.sprite.spritecollide(player2, items, True)) > 0:
+        points += 1
+        print(points)
 
     if event.type == pygame.KEYDOWN:
         if keys[pygame.K_UP]:
-            player1.rect.y -= player1.speed
+            player2.rect.y -= player2.speed
         if keys[pygame.K_DOWN]:
-            player1.rect.y += player1.speed
+            player2.rect.y += player2.speed
         if keys[pygame.K_LEFT]:
-            player1.image = pygame.transform.flip(player1_img, True, False)
-            player1.rect.x -= player1.speed
+            player2.image = pygame.transform.flip(player2_img, True, False)
+            player2.rect.x -= player2.speed
         if keys[pygame.K_RIGHT]:
-            player1.image = pygame.transform.flip(player1_img, False, False)
-            player1.rect.x += player1.speed
+            player2.image = pygame.transform.flip(player2_img, False, False)
+            player2.rect.x += player2.speed
 
 
         
